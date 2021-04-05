422facd07b9a3e5ea9528a2b43f38bb1ca84c9b6
dshinchan <dayuagrawal@gmail.com>
2021-04-06 00:02:55 +0530

    number of nodes along the longest path from the root node down to the farthest leaf node

---
+++ maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,57 @@
+/*Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+Example 3:
+
+Input: root = []
+Output: 0
+
+Example 4:
+
+Input: root = [0]
+Output: 1
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 104].
+    -100 <= Node.val <= 100 */
+
+
+//code 
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        if(root->left==NULL && root->right==NULL){
+            return 1;
+        }
+        int x=0,y=0;
+        if(root->left!=NULL){
+            x=maxDepth(root->left);
+        }
+        if(root->right!=NULL){
+            y=maxDepth(root->right);
+        }
+        return x>y?x+1:y+1;
+    }
+};
+
+
